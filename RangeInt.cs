b67e0da6f1d1a239d76d14c6c56e2266a34075bb
damasterham <cmeisterham@gmail.com>
2016-06-28 22:08:25 +0200

    Initial Commit
    +RangeProperty.cs an abstract class that functions as a ranged struct value
    +RangeInt.cs the Integer implemented version of RangeProperty.cs

---
+++ RangeInt.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModularSkills.CustomProperties
+{
+    //RangeInt<T percentilce type>
+    public class RangeInt : RangeProperty<int, float>//, IPrecantable<float>
+    {
+        public RangeInt(int max, int min) : base(max, min) { }
+
+        public RangeInt(int max) : base(max, 0) { }
+
+        protected override void SetMax(int amount)
+        {
+            if (amount < min)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            max = amount;
+
+            if (max < current)
+            {
+                Fill();
+            }
+        }
+
+        protected override void SetMin(int amount)
+        {
+            if (amount > max)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            min = amount;
+
+            if (min > current)
+            {
+                Empty();
+            }
+        }
+
+        protected override void SetCurrent(int amount)
+        {
+            if (amount < min)
+                Empty();
+            else if (amount > max)
+                Fill();
+            else
+                current = amount;
+        }
+
+        // GetCurrentPercet<T>
+        protected override float GetCurrentPercent()
+        {
+            float difference = max - min;
+            float offset = min + current;
+            return offset / difference;
+        }
+
+        public override void Add(int amount)
+        {
+            if (amount < 0)
+                return;
+
+            int cur = current;
+            cur += amount;
+            SetCurrent(cur);
+        }
+
+        public override void Subtract(int amount)
+        {
+            if (amount < 0)
+                return;
+
+            int cur = current;
+            cur -= amount;
+            SetCurrent(cur);
+
+            //Add(amount * -1);
+        }
+
+        public override void Fill()
+        {
+            current = max;
+        }
+
+        public override void Empty()
+        {
+            current = min;
+        }
+
+        //protected float GetCurrentPrercent()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ RangeProperty.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModularSkills.Objects;
+
+namespace ModularSkills
+{
+    public abstract class RangeProperty<T, U>
+    {
+        protected T current;
+        protected T min;
+        protected T max;
+
+        public RangeProperty(T max, T min)
+        {        
+            this.min = min;
+            this.max = max;
+            Fill();
+        }
+
+        //public T Current { get { return current; } }
+
+        public T Max
+        {
+            get { return max; }
+            set { SetMax(value); }
+        }
+
+        public T Min
+        {
+            get { return min; }
+            set { SetMin(value); }
+        }
+
+        public T Current
+        {
+            get
+            {
+
+                return current;
+            }
+            set
+            {
+
+                SetCurrent(value);
+            }
+        }
+        protected abstract void SetMin(T amount);
+        protected abstract void SetMax(T amount);
+        protected abstract void SetCurrent(T amount);
+        protected abstract U GetCurrentPercent();
+
+        public U CurrentPercent
+        {
+            get
+            {
+               return GetCurrentPercent();
+            }
+        }
+        public abstract void Add(T amount);
+        public abstract void Subtract(T amount);
+        public abstract void Fill();
+        public abstract void Empty();
+    }
+
+}
+
